e73e871f79f48b0967f5d1e8a7691092f5ddd0d0
Rofiq R. Murod <rofiqmurod@gmail.com>
2020-07-02 07:22:46 +0700

    Menambahkan Template AdminLTE dan Page Tugas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+	return view('isi.index');
+});
+
+Route::get('/data-tables', function() {
+	return view('isi.data');
 });
